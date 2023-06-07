f0ded258f9a7d6bfa191d709877d50b159f7076a
Showaiz <e11894showaiz@dpsrkp.net>
2023-06-07 14:39:57 +0530

    An express server

---
+++ myapp/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/number/:num', (req, res) => {
+    let isPrime = "true";
+    let num = req.params.num;
+    if(num < 0) {
+        isPrime = "negative";
+    } else if(num == 1) {
+        isPrime = 1;
+    } else if(num > 1) {
+        for (let i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = "false";
+                break;
+            }
+        }
+    }
+    
+    if (isPrime === "true") {
+        res.send(`Number ${num} is prime`);
+    } else if (isPrime === "false") {
+        res.send(`Number ${num} is composite`);
+    } else if (isPrime === "negative") {
+        res.send(`Number ${num} is negative`);
+    } else if (isPrime === 1) {
+        res.send(`Number ${num} is neither prime nor composite`);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Express Server has started on Port ${port}`);
+})
